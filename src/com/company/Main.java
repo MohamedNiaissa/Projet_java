5cc540422af1075d2264fa61f244962b9dee19cd
Jules <jules.duarte@edu.esiee-it.fr>
2021-11-24 11:27:45 +0100

    debut qui marche

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,26 +2,48 @@ package com.company;
 import java.util.Scanner;
 public class Main {
     public static void display_menu() {
-        System.out.println ( "1) Option 1\n2) Option 2\n3) Option 3" );
+        System.out.println ( "1) Create basic recruit 1\n2) Create recruit \n3) Option 3 \n4) display character \n5) quit" );
         System.out.print ( "Selection: " );
     }
     public static void main(String[] args) {
         Scanner in = new Scanner ( System.in );
-
-        display_menu();
-        switch ( in.nextInt() ) {
-            case 1:
-                System.out.println ( "You picked option 1" );
-                break;
-            case 2:
-                System.out.println ( "You picked option 2" );
-                break;
-            case 3:
-                System.out.println ( "You picked option 3" );
-                break;
-            default:
-                System.err.println ( "Unrecognized option" );
-                break;
+        Recruit recruit = new Recruit("didier",100,1000,50);
+        ListPerso list = new ListPerso();
+        boolean cont = true;
+        while(cont) {
+            display_menu();
+            switch (in.nextInt()) {
+                case 1:
+                    System.out.println(recruit);
+                    list.addListePerso(recruit);
+                    break;
+                case 2:
+                    System.out.println("entrer le nom de votre personnage: ");
+                    String name = in.next();
+                    System.out.println("choissiser un montant de dégats: ");
+                    int dmg = in.nextInt();
+                    System.out.println("choissiser un montant de point de vie: ");
+                    int lp = in.nextInt();
+                    System.out.println("choissiser un montant d'initiative: ");
+                    int ini = in.nextInt();
+                    Recruit recruit1 = new Recruit(name, dmg, lp, ini);
+                    list.addListePerso(recruit1);
+                    break;
+                case 3:
+                    System.out.println("You picked option 3");
+                    break;
+                case 4:
+                    System.out.println("quel personnage voulez vous voir");
+                    int index = in.nextInt();
+                    list.displayChar(index);
+                    break;
+                case 5:
+                    cont = false;
+                    break;
+                default:
+                    System.err.println("Unrecognized option");
+                    break;
+            }
         }
     }
 }
--- src/com/company/Recruit.java
+++ src/com/company/Recruit.java
@@ -1,5 +1,59 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recruit {
+    private String name;
+    private int damage;
+    private int lifePoints;
+    private int initiative;
+
+    public Recruit(String name, int damage, int lifePoints, int initiative){
+        this.damage = damage;
+        this.initiative = initiative;
+        this.lifePoints = lifePoints;
+        this.name = name;
+    }
+
+    public int getDamage(){
+        return this.damage;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getLifePoints(){
+        return this.lifePoints;
+    }
+    public int getInitiative(){
+        return this.initiative;
+    }
+
+    public void takeDamage(int Damage){
+        this.lifePoints -= Damage;
+    }
+
+    public String toString() {
+        return "Recruit {" +
+                "name='" + getName() + '\'' +
+                ", damage=" + getDamage() +
+                ", lifePoints=" + getLifePoints() +
+                ", initiative=" + getInitiative() +
+                '}';
+    }
+}
+
+class ListPerso {
+    List<Recruit> listePerso = new ArrayList<>();
+
+    public void addListePerso (Recruit recru){
+        listePerso.add(recru);
+    }
+
+    public void displayChar(int index){
+        System.out.println(listePerso.get(index - 1));
+    }
 
 }
+
+
